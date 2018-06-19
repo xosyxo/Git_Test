a570325a1cb664933a611b95d09a3bf075725f3c
hwyncho <hwyn.cho@gmail.com>
2018-06-19 23:26:40 +0900

    Add some files

---
+++ bye.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "myheader.h"
+
+void printHello(void) { printf("Bye World!\n");
+
---
+++ myheader.h
@@ -0,0 +1,8 @@
+#ifndef __MYHEADER_H__
+#define __MYHEADER_H__
+
+void printHello(void);
+void printBye(void);
+
+#endif
+
